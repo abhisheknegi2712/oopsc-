280fc36d56e166a7fcf4edb83a8f0839da0c7c2b
abhisheknegi2712 <55137691+abhisheknegi2712@users.noreply.github.com>
2020-07-26 14:42:50 +0530

    Add files via upload

---
+++ 1-program.cpp
@@ -0,0 +1,27 @@
+//to find greatest of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter three numbers"<<"\n";
+cin>>a>>b>>c;
+int max=0;
+if(a>max)
+{
+max=a;
+}
+if(b>max)
+{
+max=b;
+}
+if(c>max)
+{
+max=c;
+}
+cout<<"max no out of three is: ";
+cout<<max;
+return 0;
+
+
+}
